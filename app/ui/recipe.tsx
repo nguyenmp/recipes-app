818a726f2924c95a6a3b7dbf3d63502cfb639172
Mark Nguyen <contact@nguyenmp.com>
2024-07-20 09:34:53 -0700

    Fixed pnpm build where 'Type 'string' is not assignable to type 'number'.'

--- app/ui/recipe.tsx
+++ app/ui/recipe.tsx
@@ -75,7 +75,7 @@ export function EditNote(params: { note?: ShallowNote }) {
             </div>
             <div className="flex flex-row">
                 <label className="p-2" htmlFor="content_markdown">Content</label>
-                <textarea className="border bg-slate-200 p-2" rows="10" cols="30" id="content_markdown" name="content_markdown" defaultValue={params.note?.content_markdown} placeholder="TODO" />
+                <textarea className="border bg-slate-200 p-2" rows={10} cols={30} id="content_markdown" name="content_markdown" defaultValue={params.note?.content_markdown} placeholder="TODO" />
             </div>
             <button type="submit">Save {params.note ? "" : "New "} Note</button>
         </div>
