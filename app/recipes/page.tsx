42a26ad0600a17deccdbc0ff8dd91c88fca747cf
Mark Nguyen <contact@nguyenmp.com>
2024-07-22 13:49:06 -0700

    Move querying of recipes up so that it can be handled on the server and pre-rendered so recipes lists can be searched without javascript

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -4,9 +4,10 @@ import { getRecipesWithNotes } from "../lib/data";
 import { RecipeCard } from "../ui/recipe";
 import { SearchBar } from "../ui/search";
 import { Suspense } from "react";
+import { DeepRecipe } from "../lib/definitions";
 
-export async function RecipesList(params: {query?: string}) {
-  const recipes = await getRecipesWithNotes(params.query);
+export async function RecipesList(params: {recipes: DeepRecipe[]}) {
+  const recipes = params.recipes;
 
   if (recipes.length == 0) {
     return (<p>No results found</p>);
@@ -25,13 +26,14 @@ export async function RecipesList(params: {query?: string}) {
 
 export default async function Recipes({searchParams}: {searchParams: {query?: string}}) {
   const query = searchParams.query || '';
+  const recipes = await getRecipesWithNotes(query);
 
   return (
     <main>
       <Link href="/recipes/new">Create New Recipe</Link>
       <SearchBar />
       <Suspense key={query} fallback={<p>Loading...</p>}>
-        <RecipesList query={query} />
+        <RecipesList recipes={recipes} />
       </Suspense>
     </main>
   );
