4fd5d8629fc6555e0ce6a6e21d2b697d710289ad
Mark Nguyen <contact@nguyenmp.com>
2024-07-31 16:07:29 -0700

    Drop the hot-reloading workaround for the sake of type safety, also because we can't build for production otherwise

--- app/lib/embeddings_pipeline.ts
+++ app/lib/embeddings_pipeline.ts
@@ -1,36 +1,21 @@
 /**
  * Stolen straight out of the docs: https://huggingface.co/docs/transformers.js/tutorials/next#step-2-install-and-configure-transformersjs
  * 
- * This singleton allow us to avoid creating this pipeline instance every request, which ends up taking about 2 seconds on production.
+ * This singleton allow us to avoid creating this pipeline instance every request, which ends up taking about 2 seconds on production...
  */
 
-import { pipeline } from "@xenova/transformers";
+import { FeatureExtractionPipeline, pipeline } from "@xenova/transformers";
 
 // Use the Singleton pattern to enable lazy construction of the pipeline.
 // NOTE: We wrap the class in a function to prevent code duplication (see below).
-const P = () => class PipelineSingleton {
-    static task = 'embeddings';
+export default class PipelineSingletonClass {
     static model = 'Xenova/all-MiniLM-L6-v2';
-    static instance = null;
+    static instance: Promise<FeatureExtractionPipeline> | null = null;
 
-    static async getInstance(progress_callback = null) {
+    static async getInstance(progress_callback = undefined) {
         if (this.instance === null) {
-            this.instance = pipeline(this.task, this.model, { progress_callback });
+            this.instance = pipeline('embeddings', this.model, { progress_callback });
         }
         return this.instance;
     }
 }
-
-let PipelineSingleton;
-if (process.env.NODE_ENV !== 'production') {
-    // When running in development mode, attach the pipeline to the
-    // global object so that it's preserved between hot reloads.
-    // For more information, see https://vercel.com/guides/nextjs-prisma-postgres
-    if (!global.PipelineSingleton) {
-        global.PipelineSingleton = P();
-    }
-    PipelineSingleton = global.PipelineSingleton;
-} else {
-    PipelineSingleton = P();
-}
-export default PipelineSingleton;
