13f7fa066216fc4baf2607e4cf3fe17726d8bfbe
Mark Nguyen <contact@nguyenmp.com>
2024-08-07 20:37:35 -0700

    Group suggested terms tests together

--- tests/recipes.spec.ts
+++ tests/recipes.spec.ts
@@ -40,37 +40,39 @@ test('clicking a specific recipe goes to the correct recipe', async ({ page }) =
   await expect(page.getByText('Note, it uses cornmeal instead')).toBeVisible();
 });
 
-async function testForSuggestedTerm(page: Page, term: string, expected_query: string) {
-  // We use {force: true} here because this test fails on WebKit almost all the time
-  await page.getByText(`+${term}`, {exact: true}).click({force: true});
-  await expect(page).toHaveURL(new RegExp(`[=+]${term}[+]?`));
-  await expect(page.getByPlaceholder('Search here...')).toHaveValue(expected_query);
-}
+test.describe('suggested terms', () => {
+  async function testForSuggestedTerm(page: Page, term: string, expected_query: string) {
+    // We use {force: true} here because this test fails on WebKit almost all the time
+    await page.getByText(`+${term}`, {exact: true}).click({force: true});
+    await expect(page).toHaveURL(new RegExp(`[=+]${term}[+]?`));
+    await expect(page.getByPlaceholder('Search here...')).toHaveValue(expected_query);
+  }
 
-test('levenshtein suggestions work and we reduce query supersets', async ({ page }) => {
-  await page.goto(TEST_URL);
-  await page.getByPlaceholder('Search here...').fill('tomatoes and eggs');
-  await page.getByText('Search', {exact: true}).click();
-  await testForSuggestedTerm(page, 'tomato', 'tomato and eggs');
-});
+  test('levenshtein suggestions work and we reduce query supersets', async ({ page }) => {
+    await page.goto(TEST_URL);
+    await page.getByPlaceholder('Search here...').fill('tomatoes and eggs');
+    await page.getByText('Search', {exact: true}).click();
+    await testForSuggestedTerm(page, 'tomato', 'tomato and eggs');
+  });
 
-test('stored embeddings suggestions work', async ({ page }) => {
-  await page.goto(TEST_URL);
-  await page.getByPlaceholder('Search here...').fill('wrapper');
-  await page.getByText('Search').click();
-  await testForSuggestedTerm(page, 'blanket', 'wrapper blanket');
-});
+  test('stored embeddings suggestions work', async ({ page }) => {
+    await page.goto(TEST_URL);
+    await page.getByPlaceholder('Search here...').fill('wrapper');
+    await page.getByText('Search').click();
+    await testForSuggestedTerm(page, 'blanket', 'wrapper blanket');
+  });
 
-test('dynamic (realtime) embeddings suggestions work', async ({ page }) => {
-  await page.goto(TEST_URL);
-  await page.getByPlaceholder('Search here...').fill('lobsters');
-  await page.getByText('Search').click();
+  test('dynamic (realtime) embeddings suggestions work', async ({ page }) => {
+    await page.goto(TEST_URL);
+    await page.getByPlaceholder('Search here...').fill('lobsters');
+    await page.getByText('Search').click();
 
-  // It is crucial that lobsters does not resolve to a page, meaning it won't
-  // be a known word in our db thus we are finding this related terms through
-  // dynamic (realtime) embedding generation
-  await expect(page.getByText('No results found')).toBeVisible();
-  await testForSuggestedTerm(page, 'lobster', 'lobster');
+    // It is crucial that lobsters does not resolve to a page, meaning it won't
+    // be a known word in our db thus we are finding this related terms through
+    // dynamic (realtime) embedding generation
+    await expect(page.getByText('No results found')).toBeVisible();
+    await testForSuggestedTerm(page, 'lobster', 'lobster');
+  });
 });
 
 test('recipes front page is cached between loads, and reset when cache is cleared', async ({page}) => {
