fb45b313733ef587e780f38d3bbce2dd5358b7e8
Mark Nguyen <contact@nguyenmp.com>
2024-08-09 00:20:12 -0700

    Added a very simple logging middleware

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+import type { NextFetchEvent, NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+    console.log(`Starting handling of request ${request.url}`);
+
+    const response = NextResponse.next();
+
+    console.log(`Ending handling of request ${request.url} with ${response.status}`)
+
+    return response;
+}
+
+export const config = {}
